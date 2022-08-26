3b18af3077913763ae2a3d21e909de016a12cb39
Marcin <marcin.t.kozak@gmail.com>
2022-08-26 21:29:20 +0100

    solutions update

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class UpdatedApp {
     public static void main(String[] args) {
 
-        System.out.println("I'm"+ "\n" +"updated" + "\n" + "can"+ "\n" +"be"+ "\n" +"compiled."+ "\n" +"And"+ "\n" +"can"+ "\n" +"write"+ "\n" +"to"+ "\n" +"console");
+        System.out.println("I'm updated can be compiled. And can write to console");
     }
 }
